980040f88b05ae4b89d9399ec410ab9604d97353
JoseD <95452898+JoseD2021@users.noreply.github.com>
2024-11-22 21:17:07 -0500

    g

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Configuration Parameters
 CONFIG_PARAMS = {
-    'SERVER_IP_ADDRESS': '172.17.4.109',
+    'SERVER_IP_ADDRESS': '172.17.4.1095',
     'SERVER_PORT': 8082,
     'SERVER_MAX_CLIENTS': 50,
     'EXIT_MESSAGE': 'exit'
